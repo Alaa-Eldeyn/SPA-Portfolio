a28685473592df447544f25cbf6d43c4c4c9573d
Alaa Eldeyn <93477209+Alaa-Eldeyn@users.noreply.github.com>
2024-02-17 00:43:44 +0200

    Update CV.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -82,7 +82,7 @@ const Home = () => {
                   whileInView={"visible"}
                   transition={{ delay: 0.38 }}
                   exit={{ x: "-100%", opacity: 0 }}
-                  href="https://drive.google.com/uc?export=download&id=1W7VB2gqn1VotVg_JN6rz9eI3_Meg3zx2"
+                  href="https://drive.google.com/uc?export=download&id=12MQK5O5gQDCo_6IAo7z7FDCVlACtl-nC"
                 >
                   <button className="button rounded" type="button">
                     <span className="button__text">Download CV</span>
